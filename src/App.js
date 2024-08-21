a398802c6dff0f8c2bdc6eb35f5889e87c8b8e2a
Aashish Yadav <yadavaashish276@gmail.com>
2024-08-21 12:47:57 +0530

    CRA & Tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-
+import './index.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-3xl text-green-500">
+      {/* <h1 className="text-green-50">Aashish Yadav</h1> */}
+      Hello
     </div>
   );
 }
